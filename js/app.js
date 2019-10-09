7c7cc669567bd9d81227f27a7a77649ccdb53f4f
LindsayPeltier <lindsaypeltier@gmail.com>
2019-10-08 23:31:50 -0700

    added chart.js script tags to html and code to app.js

--- js/app.js
+++ js/app.js
@@ -58,12 +58,15 @@ BusmallImage.allImages = []; //array to store images
 //}
 
 //helper functions
-var renderNewImages = function(leftIndex, middleIndex, rightIndex){ //renders three images on the screen
+
+//renders three images on the screen
+var renderNewImages = function(leftIndex, middleIndex, rightIndex){
   leftImageTag.src = BusmallImage.allImages[leftIndex].imgURL;
   middleImageTag.src = BusmallImage.allImages[middleIndex].imgURL;
   rightImageTag.src = BusmallImage.allImages[rightIndex].imgURL;
 };
 
+//Randomizer
 var pickNewImages = function(){
   var leftIndex = Math.ceil(Math.random() * BusmallImage.allImages.length - 1);
 
@@ -73,10 +76,10 @@ var pickNewImages = function(){
   } while((rightIndex === leftIndex) || (rightIndex === middleIndex) || (leftIndex === middleIndex));
 
   leftImgOnThePage = BusmallImage.allImages[leftIndex];
-  rightImgOnThePage = BusmallImage.allImages[rightIndex];
   middleImgOnThePage = BusmallImage.allImages[middleIndex];
+  rightImgOnThePage = BusmallImage.allImages[rightIndex];
 
-  renderNewImages(leftIndex, rightIndex, middleIndex);
+  renderNewImages(leftIndex, middleIndex, rightIndex);
 };
 
 //Event Handler
@@ -126,5 +129,42 @@ function handleReset(){
 
 pickNewImages();
 
-
+//chart
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
 
